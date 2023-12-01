28bfc79595adbfb0b3745f38bc1971e4839f49c3
omeatai <omeatai@gmail.com>
2023-11-30 21:36:36 -0700

    3. Setup Async Handler
    3. Setup Async Handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,10 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 module.exports = { authUser };
