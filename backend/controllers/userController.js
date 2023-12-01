f7d7c35dfb7f255b46e9cd5a81134873e1f51a3c
omeatai <omeatai@gmail.com>
2023-12-01 00:26:08 -0700

    5. User Controller Functions and Routes
    5. User Controller Functions and Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,44 @@
 const asyncHandler = require("express-async-handler");
 
-// @desc Auth user/set token
+// @desc Auth/Login user/set token
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error("Testing Error handling....");
-  //   res.status(200).json({ message: "Auth User" });
+  res.status(200).json({ message: "Auth User" });
 });
 
-module.exports = { authUser };
+// @desc Register user
+// route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc Get User Profile
+// route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get User Profile" });
+});
+
+// @desc Update User Profile
+// route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+module.exports = {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,32 @@
-// import express from "express"; //   "type": "module", in package.json
 const express = require("express");
 const router = express.Router();
 
-const { authUser } = require("../controllers/userController");
+const {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/userController");
 
+// @desc Register user
+// route POST /api/users
+// @access Public
+router.post("/", registerUser);
+
+// @desc Auth/Login user/set token
+// route POST /api/users/auth
+// @access Public
 router.post("/auth", authUser);
 
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+router.post("/logout", logoutUser);
+
+// @desc Get User Profile && Update User Profile
+// route GET /api/users/profile &&  PUT /api/users/profile
+// @access Private
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+
 module.exports = router;
-// export default router; "type": "module", in package.json
