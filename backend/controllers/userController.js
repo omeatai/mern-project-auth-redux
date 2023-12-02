c37ed9d70b5fca7da7447a6539959c4a7d3b649e
omeatai <omeatai@gmail.com>
2023-12-02 15:53:39 -0700

    11. Logout User Logic
    11. Logout User Logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 // @desc Get User Profile
