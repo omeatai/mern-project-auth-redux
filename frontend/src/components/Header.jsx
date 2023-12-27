db3c5caebcb4f9b23a2a06b458c57ad5c59569df
omeatai <omeatai@gmail.com>
2023-12-27 09:37:08 -0700

    23. Setup Redux Login Functionality
    23. Setup Redux Login Functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,14 @@
-// import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     return (
         <header>
@@ -16,22 +21,36 @@ const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <FaSignInAlt /> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
+                            { userInfo ? (
+                                <NavDropdown title={ userInfo.name } id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <>
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link>
+                                            <FaSignInAlt /> Sign In
+                                        </Nav.Link>
+                                    </LinkContainer>
 
-                            <LinkContainer to='/register'>
-                                <Nav.Link>
-                                    <FaSignOutAlt /> Sign Up
-                                </Nav.Link>
-                            </LinkContainer>
+                                    <LinkContainer to='/register'>
+                                        <Nav.Link>
+                                            <FaSignOutAlt /> Sign Up
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                </>
+                            ) }
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
-        </header>
+            </Navbar >
+        </header >
     );
 };
 
