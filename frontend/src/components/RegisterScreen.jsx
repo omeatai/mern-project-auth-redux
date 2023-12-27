f177d7f1062df767b616d03da6a7505a56ac1e9a
omeatai <omeatai@gmail.com>
2023-12-27 12:12:56 -0700

    25. Setup Redux Register Functionality
    25. Setup Redux Register Functionality

--- frontend/src/components/RegisterScreen.jsx
+++ frontend/src/components/RegisterScreen.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import FormContainer from './FormContainer'
+import Loader from '../components/Loader';
 
 const RegisterScreen = () => {
     const [name, setName] = useState('');
@@ -9,9 +14,33 @@ const RegisterScreen = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [register, { isLoading }] = useRegisterMutation();
+
+    const { userInfo } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate('/');
+        }
+    }, [navigate, userInfo]);
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        console.log('submit');
+
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+        } else {
+            try {
+                const res = await register({ name, email, password }).unwrap();
+                dispatch(setCredentials({ ...res }));
+                navigate('/');
+            } catch (err) {
+                toast.error(err?.data?.message || err.error);
+            }
+        }
     }
 
     return (
@@ -62,12 +91,13 @@ const RegisterScreen = () => {
                     Sign Up
                 </Button>
 
-                <Row className='py-3'>
-                    <Col>
-                        Already have an Account? <Link to='/login'>Login</Link>
-                    </Col>
-                </Row>
+                { isLoading && <Loader /> }
             </Form>
+            <Row className='py-3'>
+                <Col>
+                    Already have an Account? <Link to='/login'>Login</Link>
+                </Col>
+            </Row>
         </FormContainer>
     )
 }
