9b2e7bac1e9fe5424b476b8b69713e9f6352febf
omeatai <omeatai@gmail.com>
2023-12-28 15:08:31 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,43 +6,40 @@ const path = require('path');
 const express = require("express");
 const port = process.env.PORT || 8000;
 
-// const cors = require("cors");
-
-
-// const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
-// const userRoutes = require("./routes/userRoutes");
-// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// app.use(cookieParser());
+app.use("/api/users", userRoutes);
 
-// app.use("/api/users", userRoutes);
+// app.get("/", (req, res) => res.send("Server is ready"));
 
-app.get("/", (req, res) => res.send("Server is ready"));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   const __dirname = path.resolve();
-//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
-
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
