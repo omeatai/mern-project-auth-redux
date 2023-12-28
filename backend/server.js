339ee31ea910efa2d11dd6841becdeb7c019a211
omeatai <omeatai@gmail.com>
2023-12-28 14:54:07 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
+// dotenv.config();
+require("dotenv").config();
+
 const path = require('path');
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
 
-connectDB();
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+
+// const cookieParser = require("cookie-parser");
+// const connectDB = require("./config/db");
+// const userRoutes = require("./routes/userRoutes");
+// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+// connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
@@ -36,8 +40,8 @@ app.get("/", (req, res) => res.send("Server is ready"));
 //   });
 // }
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
